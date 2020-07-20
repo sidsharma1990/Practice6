5489fddf28e2cd391d5979569bef261cb1d40fe2
Sandeep Sharma <59616811+sidsharma1990@users.noreply.github.com>
2020-07-20 15:10:49 +0530

    Add files via upload

---
+++ Jay Basic.py
@@ -0,0 +1,101 @@
+# Jay Basic
+a = 10
+b = 50
+ ''
+ ""
+'Hello World'
+"Hello World"
+
+name = 'ML'
+
+'Hello's world'
+"Hello's World"
+'Hello\'s world'
+
+6+6
+print ('6+6')
+print ('6'+'6')
+print ('6'+6)
+print (6.0+6)
+
+flaot (master value) 123.5, 6.0, 9.00000001
+int = 1,2,3,4
+
+# Conversion
+'''eiugfhseouf
+kpajfoseijfio
+aoifjaeoifhseoufh
+jfpiajoifjaeiojf
+afpjaifjsepifjiose
+poasjfpodjvpiij
+paojposkvo'''
+a = '5'
+print (int(a))
+
+b = int (a)
+c = float(a)
+
+d = str(b)
+
+b = '1.22'
+print (float(b))
+print (int(b))
+
+print (int(90.56))
+
+print (type(a))
+print (type(3.5))
+print (type(3))
+
+print (9*9)
+print (9**3)
+print (9**9)
+print (pow(9, 9))
+
+print ('Jai' + b)
+print ('Jai' + c)
+print ('Jai', c)
+
+print ('Jai' + str(c))
+
+
+print ('My name is python' + 'and age is' + str(29))
+print ('My name is python', 'and age is', 29)
+
+print (5<3)
+
+a = 10
+b = 20 
+c = 30
+
+a = b = 10
+a,b,c = 5,15,25
+
+# Splitting
+name = 'Hello World'
+print (name.split(' '))
+print (name.split('o'))
+
+print (' ')
+print ('Jai', name.split(' ')[1])
+
+# Positive indexing
+name[8]
+print (len(name))
+
+# -ve indexing
+print(name[-2])
+
+
+
+top 10
+0-9
+
+
+
+
+
+
+
+
+
